8934a6264ded9955079507652014919ccf94106f
Matheus Henrique <32910717+matheushenrique1@users.noreply.github.com>
2021-03-14 11:22:09 -0300

    refactor: remove comments

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,7 +15,6 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
     if(!newTaskTitle) return;
 
     const newTask = {
@@ -29,13 +28,11 @@ export function TaskList() {
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
     const updateTask = tasks.map(task => task.id === id ? { ...task, isComplete: !task.isComplete } : task);
     setTasks(updateTask);
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
     const allTasks = tasks.filter(task => task.id !== id);
     setTasks(allTasks);
   }
